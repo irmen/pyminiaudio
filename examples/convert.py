cf63ec357ec32cad565072727478d905c511d8f3
Irmen de Jong <irmen@razorvine.net>
2019-07-04 04:49:56 +0200

    conversion

--- examples/convert.py
+++ examples/convert.py
@@ -11,7 +11,7 @@ src = miniaudio.decode_file(samples_path("music.ogg"))
 print("Source: {src.nchannels} ch, {src.sample_rate} hz, {src.sample_format_name}, {src.num_frames} frames"
       .format(src=src))
 
-result = miniaudio.DecodedSoundFile("result", 2, 12000, 1, miniaudio.SampleFormat.UNSIGNED8, array.array('b'))
+result = miniaudio.DecodedSoundFile("result", 1, 8000, 1, miniaudio.SampleFormat.UNSIGNED8, array.array('b'))
 converted_frames = miniaudio.convert_frames(src.sample_format, src.nchannels, src.sample_rate, src.samples.tobytes(),
                                             result.sample_format, result.nchannels, result.sample_rate)
 
