061695f26a42749a86277ece8dbd50a8785a9fc2
Cameron Maske <cameronmaske@gmail.com>
2019-08-06 21:41:06 +0200

    Add running flag

--- tests/test_miniaudio.py
+++ tests/test_miniaudio.py
@@ -22,33 +22,39 @@ class MiniaudioTests(unittest.TestCase):
         next(gen)
         capture.start(gen, stop_callback)
 
+        self.assertTrue(capture.running)
         # Force a stop.
         miniaudio.lib.ma_device_stop(capture._device)
 
         stop_callback.assert_called_once()
+        self.assertFalse(capture.running)
 
     def test_stop_callback_playback(self):
         stop_callback = mock.Mock()
 
-        capture = miniaudio.PlaybackDevice()
+        playback = miniaudio.PlaybackDevice()
         gen = dummy_generator()
         next(gen)
-        capture.start(gen, stop_callback)
+        playback.start(gen, stop_callback)
 
+        self.assertTrue(playback.running)
         # Force a stop.
-        miniaudio.lib.ma_device_stop(capture._device)
+        miniaudio.lib.ma_device_stop(playback._device)
 
         stop_callback.assert_called_once()
+        self.assertFalse(playback.running)
 
     def test_stop_callback_duplex(self):
         stop_callback = mock.Mock()
 
-        capture = miniaudio.DuplexStream()
+        duplex = miniaudio.DuplexStream()
         gen = dummy_generator()
         next(gen)
-        capture.start(gen, stop_callback)
+        duplex.start(gen, stop_callback)
 
+        self.assertTrue(duplex.running)
         # Force a stop.
-        miniaudio.lib.ma_device_stop(capture._device)
+        miniaudio.lib.ma_device_stop(duplex._device)
 
         stop_callback.assert_called_once()
+        self.assertFalse(duplex.running)
