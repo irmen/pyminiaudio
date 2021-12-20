24d1c44bfa67f26842a9bfd13d67d5dbe7938170
Irmen de Jong <irmen@razorvine.net>
2021-12-20 22:08:48 +0100

    Simplify compile flags (#46)
    Co-authored-by: Andrew Williams <andy@tensixtyone.com>

--- build_ffi_module.py
+++ build_ffi_module.py
@@ -11,7 +11,7 @@ Software license: "MIT software license". See http://opensource.org/licenses/MIT
 
 
 import os
-import platform
+import shlex
 from cffi import FFI
 
 miniaudio_include_dir = os.getcwd()
@@ -793,14 +793,8 @@ if os.name == "posix":
     compiler_args = ["-g1", "-O3", "-ffast-math"]
     libraries = ["m", "pthread", "dl"]
 
-    # determine appropriate compiler tuning options
-    if platform.system() == "Darwin":
-        compiler_args += []         # apple compiler doesn't seem to like any tune/cpu options
-    elif "ppc64" in platform.system():
-        compiler_args += ["-mcpu=native"]
-    else:
-        compiler_args += ["-mtune=native", "-march=native"]
-
+    if "PYMINIAUDIO_EXTRA_CFLAGS" in os.environ:
+        compiler_args += shlex.split(os.environ.get("PYMINIAUDIO_EXTRA_CFLAGS", ""))
 
 ffibuilder.set_source("_miniaudio", """
     #include <stdint.h>
