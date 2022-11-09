45eeff733a1b9cc87666d71937741a198f1e84de
Irmen de Jong <irmen@razorvine.net>
2022-11-09 19:59:56 +0100

    release 1.54 - update to miniaudio 0.11.11

--- miniaudio.py
+++ miniaudio.py
@@ -5,7 +5,7 @@ Author: Irmen de Jong (irmen@razorvine.net)
 Software license: "MIT software license". See http://opensource.org/licenses/MIT
 """
 
-__version__ = "1.53"
+__version__ = "1.54"
 
 
 import abc
